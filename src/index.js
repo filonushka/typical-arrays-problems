c76f1c40b6d5310ea32d7b02633bdb59a11f96df
filonushka <lero4ka.filonova@mail.ru>
2021-02-24 00:59:44 +0300

    feat: finish the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+exports.min = function min(array) {
+    if (Array.isArray(array) === true && array.length !== 0) {
+        var min = Math.min.apply(null, array);
+        return min;
+    } else {
+        return 0;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (Array.isArray(array) === true && array.length !== 0) {
+        var max = Math.max.apply(null, array);
+        return max;
+    } else {
+        return 0;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (Array.isArray(array) === true && array.length !== 0) {
+        var avg = array.reduce((a, b) => a + b, 0) / array.length;
+        return avg;
+    } else {
+        return 0;
+    }
+};
